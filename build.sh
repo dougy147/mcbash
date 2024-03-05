629008f715320067aa49efd4dbea58f786e10645
dougy147 <16362303+dougy147@users.noreply.github.com>
2024-03-05 23:20:19 +0100

    build.sh finally shellchecked

--- build.sh
+++ build.sh
@@ -7,7 +7,8 @@ CONF_DEST="\${HOME}/.config/mcbash"
 ALT_CONF_DEST="/etc"
 
 # Pre-processing the build
-if [[ -d ./build ]]; then
+if [ -d ./build ]
+then
 	rm -r ./build 2>/dev/null && mkdir ./build 2>/dev/null
 else
 	mkdir ./build 2>/dev/null
@@ -15,23 +16,22 @@ fi
 
 # Create the monolithic program mcbash
 touch ./build/mcbash
-echo "#!/bin/bash" >> ./build/mcbash
-echo "CONF_DEST=\"${CONF_DEST}\"" >> ./build/mcbash
-echo "ALT_CONF_DEST=\"${ALT_CONF_DEST}\"" >> ./build/mcbash
+{ echo "#!/bin/bash"; echo "CONF_DEST=\"${CONF_DEST}\""; echo "ALT_CONF_DEST=\"${ALT_CONF_DEST}\""; } >> ./build/mcbash
 
 # Append all OPTIONAL configuration files to mcbash
-for conf_file in ./misc/*.conf; do cat "${conf_file}" | sed '/^#!\/bin\/\(ba\)\?sh/d ; /^#.*/d' >> ./build/mcbash; done
+for conf_file in ./misc/*.conf; do sed '/^#!\/bin\/\(ba\)\?sh/d ; /^#.*/d' "${conf_file}" >> ./build/mcbash; done
 
-# Concatenate all functions into mcbash (filename must be in correct order) 
-for fun in ./func/*; do cat "${fun}" | sed '/^#!\/bin\/\(ba\)\?sh/d ; /^#.*/d' >> ./build/mcbash; done
+# Concatenate all functions into mcbash (filename must be in correct order)
+for fun in ./func/*; do sed '/^#!\/bin\/\(ba\)\?sh/d ; /^#.*/d' "${fun}" >> ./build/mcbash; done
 
 # Append infos to the end of the program
 while read -r lines; do
-	if [[ "${lines}" == "" ]]; then
-		echo -e "\n" >> ./build/mcbash;
+	if [ "${lines}" = "" ]
+	then
+		echo "" >> ./build/mcbash;
 	fi;
-	echo -e "# ${lines}" >> ./build/mcbash;
-done < <(cat ./misc/infos.txt)
+	echo "# ${lines}" >> ./build/mcbash;
+done < ./misc/infos.txt
 
 # Read version of mcbash to insert it in program
 # TODO
